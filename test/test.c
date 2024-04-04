57e4e27e80390c3772b48337abc964f7aab6592e
RohanVDvivedi <rohandvivedi@gmail.com>
2024-04-04 23:21:57 +0530

    test cases to test left and right shift

--- test/test.c
+++ test/test.c
@@ -1,9 +1,22 @@
-#include<capp.h>
-#include<print_temp.h>
+#include<large_uint.h>
+
+#include<stdio.h>
 
 int main()
 {
-	temp t = {.s = 7, .str = "Hello World"};
-	print_temp_test(t);
-	return 0;
+	large_uint hx = {0x123456789abcdeff,0x2929292929292929};
+
+	for(uint32_t s = 0; s <= LARGE_UINT_BIT_WIDTH + 4; s += 4)
+	{
+		print_large_uint(left_shift_large_uint(hx, s));
+		printf("\n");
+	}
+	printf("\n");
+
+	for(uint32_t s = 0; s <= LARGE_UINT_BIT_WIDTH + 4; s += 4)
+	{
+		print_large_uint(right_shift_large_uint(hx, s));
+		printf("\n");
+	}
+	printf("\n");
 }
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,2 +1,2 @@
-gcc ./test.c -o test.out -lcapp
+gcc ./test.c -o test.out -lserint
 ./test.out
