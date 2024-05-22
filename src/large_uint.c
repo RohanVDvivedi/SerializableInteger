2840684c1293c3ab9f6d5423dfed65e9ddd7b0b3
RohanVDvivedi <rohandvivedi@gmail.com>
2024-05-22 22:45:04 +0530

    implemented logic for multiplying limbs of large_uint

--- src/large_uint.c
+++ src/large_uint.c
@@ -225,6 +225,47 @@ int sub_large_uint_underflow_safe(large_uint* res, large_uint a, large_uint b)
 	return 1;
 }
 
+// the operation performed here is (a * b) + carry_in
+// it returns carry_out and result in res
+static uint64_t mul_limbs_with_carry(uint64_t* res, uint64_t a, uint64_t b, uint64_t carry)
+{
+	uint64_t a_0 = a & ((UINT64_C(1) << 32) - UINT64_C(1)); // upper 32 bits of a
+	uint64_t a_1 = a >> 32; // lower 32 bits of a
+
+	// similarly extract upper and lower 32 bits of b
+	uint64_t b_0 = b & ((UINT64_C(1) << 32) - UINT64_C(1));
+	uint64_t b_1 = b >> 32;
+
+	// a_0 and b_0's product will always go to res
+	// a_1 and b_1's product will always go to carry_out
+	(*res) = a_0 * b_0;
+	uint64_t carry_out = a_1 * b_1;
+
+	// add carry_in to the (carry_out, res) pair
+	{
+		carry_out += will_unsigned_sum_overflow(uint64_t, (*res), carry_in);
+		(*res) += carry_in;
+	}
+
+	// process this cross product
+	uint64_t temp = a_0 * b_1;
+	{
+		uint64_t ct = will_unsigned_sum_overflow(uint64_t, (*res), (temp << 32));
+		r_0 += (temp << 32);
+		r_1 += ((temp >> 32) + ct);
+	}
+
+	// process this cross product
+	temp = a_1 * b_0;
+	{
+		uint64_t ct = will_unsigned_sum_overflow(uint64_t, (*res), (temp << 32));
+		r_0 += (temp << 32);
+		r_1 += ((temp >> 32) + ct);
+	}
+
+	return carry_out;
+}
+
 #include<serial_int.h>
 
 void serialize_large_uint(void* bytes, uint32_t bytes_size, large_uint l)
