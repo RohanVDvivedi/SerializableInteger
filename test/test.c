7554990b023d086b8a932fdb2702187ccaafa9a6
RohanVDvivedi <rohandvivedi@gmail.com>
2024-05-26 22:08:01 +0530

    testing uint128

--- test/test.c
+++ test/test.c
@@ -1,64 +1,66 @@
-#include<large_uint.h>
+#include<large_uints.h>
 
 #include<stdio.h>
 
+
+
 int main()
 {
-	large_uint hx = {0x123456789abcdeff,0x2929292929292929};
+	uint128 hx = {0x123456789abcdeff,0x2929292929292929};
 
-	for(uint32_t s = 0; s <= LARGE_UINT_BIT_WIDTH + 4; s += 4)
+	for(uint32_t s = 0; s <= get_bit_width_uint128() + 4; s += 4)
 	{
-		print_large_uint(left_shift_large_uint(hx, s));
+		print_uint128(left_shift_uint128(hx, s));
 		printf("\n");
 	}
 	printf("\n");
 
-	for(uint32_t s = 0; s <= LARGE_UINT_BIT_WIDTH + 4; s += 4)
+	for(uint32_t s = 0; s <= get_bit_width_uint128() + 4; s += 4)
 	{
-		print_large_uint(right_shift_large_uint(hx, s));
+		print_uint128(right_shift_uint128(hx, s));
 		printf("\n");
 	}
 	printf("\n");
 
-	for(uint32_t b = 0; b <= LARGE_UINT_BIT_WIDTH; b++)
+	for(uint32_t b = 0; b <= get_bit_width_uint128(); b++)
 	{
-		print_large_uint(get_bitmask_lower_n_bits_set(b));
+		print_uint128(get_bitmask_lower_n_bits_set_uint128(b));
 		printf("\n");
 	}
 	printf("\n");
 
-	large_uint max_sum_res;
-	uint64_t carry = add_large_uint(&max_sum_res, LARGE_UINT_MAX, LARGE_UINT_MAX);
-	print_large_uint(LARGE_UINT_MAX);printf(" * 2 = (%lu) ", carry);
-	print_large_uint(max_sum_res);printf("\n\n");
+	uint128 max_sum_res;
+	uint64_t carry = add_uint128(&max_sum_res, get_max_uint128(), get_max_uint128());
+	print_uint128(get_max_uint128());printf(" * 2 = (%lu) ", carry);
+	print_uint128(max_sum_res);printf("\n\n");
 
-	large_uint a = {0xabcdef1234567890, 0x908765abcdfecdef};
-	large_uint b = {0xfe5678cde6543210, 0x987655678efabcba};
+	uint128 a = {0xabcdef1234567890, 0x908765abcdfecdef};
+	uint128 b = {0xfe5678cde6543210, 0x987655678efabcba};
 
-	large_uint res0;
-	large_uint res1 = mul_large_uint(&res0, a, b);
+	uint128 res0;
+	uint128 res1 = mul_uint128(&res0, a, b);
 
-	print_large_uint(a);
+	print_uint128(a);
 	printf(" * ");
-	print_large_uint(b);
+	print_uint128(b);
 	printf("\n");
 	printf("= ");
-	print_large_uint(res1);
-	print_large_uint(res0);
+	print_uint128(res1);
+	print_uint128(res0);
 	printf("\n\n");
 
-	large_uint dividend  = {0xabcdef1234567890, 0x9087654321abcdef};
-	large_uint divisor   = {0xabcd, 0x1234};
-	large_uint quotient  = LARGE_UINT_ZERO;
-	large_uint remainder = div_large_uint(&quotient, dividend, divisor);
-	print_large_uint(dividend);
+	uint128 dividend  = {0xabcdef1234567890, 0x9087654321abcdef};
+	uint128 divisor   = {0xabcd, 0x1234};
+	uint128 quotient  = get_0_uint128();
+	uint128 remainder = div_uint128(&quotient, dividend, divisor);
+	print_uint128(dividend);
 	printf(" / ");
-	print_large_uint(divisor);
+	print_uint128(divisor);
 	printf("\n");
 	printf("= ");
-	print_large_uint(quotient);
+	print_uint128(quotient);
 	printf("(");
-	print_large_uint(remainder);
+	print_uint128(remainder);
 	printf(")\n\n");
 
 	return 0;
