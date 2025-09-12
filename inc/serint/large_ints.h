5eadfcf60af13d7b22155773bf100d56566570db
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-13 01:48:15 +0530

    using large_uint to make large_int

---
+++ inc/serint/large_ints.h
@@ -0,0 +1,24 @@
+#ifndef LARGE_INTS_H
+#define LARGE_INTS_H
+
+#include<serint/large_uints.h>
+
+// int128
+
+declarations_large_int(uint128)
+
+definitions_large_int(uint128)
+
+// int256
+
+declarations_large_int(uint256)
+
+definitions_large_int(uint256)
+
+// int512
+
+declarations_large_int(int512)
+
+definitions_large_int(int512)
+
+#endif
