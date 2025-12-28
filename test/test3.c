8a7610d7d8fc655b4c19356de423871ed39d9829
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-28 21:07:34 +0530

    added more test cases

--- test/test3.c
+++ test/test3.c
@@ -1,7 +1,24 @@
-#include<serint/float_parts.h>
+#include<serint/double_parts.h>
 
 int main()
 {
-	
+	double_parts p;
+
+	p = decompose_double(0.5);
+
+	p = decompose_double(-0.5);
+
+	p = decompose_double(50);
+
+	p = decompose_double(-50);
+
+	p = decompose_double(125969.12345);
+
+	p = decompose_double(-125969.12345);
+
+	p = decompose_double(0.0);
+
+	p = decompose_double(-0.0);
+
 	return 0;
 }
