6f1f975c639bbb2b9a62a64b9a87c0d87fa8d836
RohanVDvivedi <rohandvivedi@gmail.com>
2026-06-13 03:07:45 +0530

    minor bug fix

--- inc/serint/large_int.h
+++ inc/serint/large_int.h
@@ -254,7 +254,7 @@
 	{                                                                                                                                                       \
 		int msb = get_sign_bit_ ## large_int(a);                                                                                                            \
 		if(s >= get_bit_width_ ## large_int())                                                                                                              \
-			return (msb) ? get_max_ ## large_int() : get_0_ ## large_int();                                                                                 \
+			return (msb) ? (large_int){get_max_ ## large_uint()} : get_0_ ## large_int();                                                                   \
 		a = (large_int){right_shift_ ## large_uint(a.raw_uint_value, s)};                                                                                   \
 		if(msb)                                                                                                                                             \
 			a =  bitwise_or_ ## large_int(a, bitwise_not_ ## large_int(get_bitmask_lower_n_bits_set_ ## large_int(get_bit_width_ ## large_int() - s)));     \
